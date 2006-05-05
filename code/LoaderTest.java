83d8e4d62889e5a38a5c3e4b02acff395f3a4ed3
David Jones <drj@ravenbrook.com>
2006-05-05 17:06:10 +0100

    Jili: LoaderTest1 has debug info.

--- code/LoaderTest.java
+++ code/LoaderTest.java
@@ -13,6 +13,10 @@ import java.io.InputStream;
 //   luac -s - <<'EOF'
 //   return 99
 //   EOF
+// LoaderTest1.luc - as LoaderTest0.luc but with debug info:
+//    luac - <<'EOF'
+//    return 99
+//  EOF
 
 /**
  * J2MEUnit tests for Jili's internal Loader class.  DO NOT SUBCLASS.  public
@@ -31,35 +35,53 @@ public class LoaderTest extends TestCase {
   }
 
   /**
-   * Tests the loader.
+   * @param filename  filename without '.luc' extension.
    */
-  public void testLoader() {
-    String filename = "LoaderTest0";
+  private Proto loadFile(String filename) {
+    filename += ".luc";
     InputStream in = null;
+
+    System.out.println(filename);
+
     try {
-      in = new FileInputStream(filename + ".luc");
+      in = new FileInputStream(filename);
     } catch(Exception e) {
       e.printStackTrace();
     }
-    assertNotNull("Opened file", in);
+    assertNotNull("Opened file " + filename, in);
 
     boolean loaded = false;
     Loader loader = new Loader(in, filename);
 
+    Proto proto = null;
     try {
-      loader.undump();
+      proto = loader.undump();
       loaded = true;
     } catch(Exception e) {
       e.printStackTrace();
     }
     assertTrue("Loaded okay", loaded);
+
+    return proto;
+  }
+
+  /** Tests LoaderTest0.luc.  */
+  public void testLoader0() {
+    loadFile("LoaderTest0");
+  }
+
+  /** Tests LoaderTest1.luc.  */
+  public void testLoader1() {
+    loadFile("LoaderTest1");
   }
 
   public Test suite() {
     TestSuite suite = new TestSuite();
 
-    suite.addTest(new LoaderTest("testLoader") {
-        public void runTest() { testLoader(); } });
+    suite.addTest(new LoaderTest("testLoader0") {
+        public void runTest() { testLoader0(); } });
+    suite.addTest(new LoaderTest("testLoader1") {
+        public void runTest() { testLoader1(); } });
     return suite;
   }
 }
