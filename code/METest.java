7d1182ec99e56ec0c0e7e6092bb68323e7a6156f
David Jones <drj@ravenbrook.com>
2006-05-25 15:56:27 +0100

    Jili: Creating umbrella test METest and METestMIDlet (which runs successfully on the BlackBerry 8700).

---
+++ code/METest.java
@@ -0,0 +1,27 @@
+// $Header$
+
+// For j2meunit see http://j2meunit.sourceforge.net/
+import j2meunit.framework.Test;
+import j2meunit.framework.TestCase;
+import j2meunit.framework.TestSuite;
+
+/**
+ * Compendium of all Jili's tests that run in the J2ME environment.
+ */
+public class METest extends TestCase {
+  public Test suite() {
+    TestSuite suite = new TestSuite();
+    TestCase t;
+
+    t = new ObjectAllTest();
+    suite.addTest(t.suite());
+    t = new LoaderTest();
+    suite.addTest(t.suite());
+    t = new LuaTest();
+    suite.addTest(t.suite());
+    t = new VMTest();
+    suite.addTest(t.suite());
+
+    return suite;
+  }
+}
---
+++ code/METestMIDlet.java
@@ -0,0 +1,15 @@
+// $Header$
+
+import j2meunit.midletui.TestRunner;
+
+/**
+ * J2MEUnit TestRunner MIDlet.
+ */
+public class METestMIDlet extends TestRunner {
+  public METestMIDlet() { }
+
+  public void startApp() {
+    start(new String[] { "METest" });
+  }
+}
+
