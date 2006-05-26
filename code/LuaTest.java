fac6bb998960a472ef84c9f450406c61dbc77e88
David Jones <drj@ravenbrook.com>
2006-05-26 11:17:43 +0100

    Jili: Test for code that's in the plan.  Fails.

--- code/LuaTest.java
+++ code/LuaTest.java
@@ -6,6 +6,11 @@ import j2meunit.framework.TestCase;
 import j2meunit.framework.TestSuite;
 
 // Uses some of the same ancillary files as LoaderTest.
+// LuaTest0.luc - compiled version of (example code in the plan):
+//   a = 7
+//   b = {a, (a+1)/2, "foo"}
+//   c = b[1] .. b[3]
+//   return c
 
 /**
  * J2MEUnit tests for Jili's public API.  DO NOT SUBCLASS.  public
@@ -27,9 +32,7 @@ public class LuaTest extends TestCase {
     Lua L = new Lua();
   }
 
-  /** Helper used by testLua1. */
-  public void simpleScript(String filename) {
-    Lua L = new Lua();
+  private LuaFunction loadFile(Lua L, String filename) {
     LuaFunction f = null;
     System.out.println(filename);
     try {
@@ -38,6 +41,13 @@ public class LuaTest extends TestCase {
     } catch (Exception e) {
       e.printStackTrace();
     }
+    return f;
+  }
+
+  /** Helper used by testLua1. */
+  private void simpleScript(String filename) {
+    Lua L = new Lua();
+    LuaFunction f = loadFile(L, filename);
     assertNotNull("Loaded script", f);
     L.push(f);
     int top = L.gettop();
@@ -79,6 +89,16 @@ public class LuaTest extends TestCase {
     assertTrue("Callback got called", v[0] == MAGIC);
   }
 
+  /** Tests that the Lua script in the plan can be executed. */
+  public void testLua3() {
+    Lua L = new Lua();
+    LuaFunction f = loadFile(L, "LuaTest0.luc");
+    L.push(f);
+    L.call(0, 1);
+    System.out.println(L.value(1));
+    assertTrue("Result is 7foo", "7foo".equals(L.value(1)));
+  }
+
   public Test suite() {
     TestSuite suite = new TestSuite();
 
@@ -88,6 +108,8 @@ public class LuaTest extends TestCase {
         public void runTest() { testLua1(); } });
     suite.addTest(new LuaTest("testLua2") {
         public void runTest() { testLua2(); } });
+    suite.addTest(new LuaTest("testLua3") {
+        public void runTest() { testLua3(); } });
     return suite;
   }
 }
