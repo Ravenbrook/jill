e5de28a1dced1bf4da1c52105fea635cdcb4bbdb
David Jones <drj@ravenbrook.com>
2006-06-16 12:51:03 +0100

    Jili: Adding an Intuwave conforming (nearly) header.

--- code/Proto.java
+++ code/Proto.java
@@ -1,4 +1,18 @@
-// $Header$
+/*  $Header$
+ *  (c) Copyright 2006, Intuwave Ltd. All Rights Reserved.
+ *
+ *  Although Intuwave has tested this program and reviewed the documentation,
+ *  Intuwave makes no warranty or representation, either expressed or implied,
+ *  with respect to this software, its quality, performance, merchantability,
+ *  or fitness for a particular purpose. As a result, this software is licensed
+ *  "AS-IS", and you are assuming the entire risk as to its quality and
+ *  performance.
+ *
+ *  You are granted license to use this code as a basis for your own
+ *  application(s) under the terms of the separate license between you and
+ *  Intuwave.
+ */
+
 
 /**
  * Models a function prototype.  This class is internal to Jili and
