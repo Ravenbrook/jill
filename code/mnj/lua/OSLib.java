b8e237164397a14c8daa20c8c1bb8818f03a76ad
David Jones <drj@ravenbrook.com>
2006-07-21 11:38:54 +0100

    Jili: Most of strftime supported in os.date

--- code/mnj/lua/OSLib.java
+++ code/mnj/lua/OSLib.java
@@ -13,6 +13,9 @@
  *  Intuwave.
  */
 
+// REFERENCES
+// [C1990] "ISO Standard: Programming languages - C"; ISO 9899:1990;
+
 package mnj.lua;
 
 import java.util.Calendar;
@@ -148,7 +151,7 @@ public final class OSLib extends LuaJavaCallback {
       setfield(L, "day", c.get(Calendar.DAY_OF_MONTH));
       setfield(L, "month", canonicalmonth(c.get(Calendar.MONTH)));
       setfield(L, "year", c.get(Calendar.YEAR));
-      setfield(L, "wday", c.get(Calendar.DAY_OF_WEEK));
+      setfield(L, "wday", canonicalweekday(c.get(Calendar.DAY_OF_WEEK)));
       // yday is not supported because CLDC 1.1 does not provide it.
       // setfield(L, "yday", c.get("???"));
       if (tz.useDaylightTime())
@@ -167,8 +170,110 @@ public final class OSLib extends LuaJavaCallback {
     }
     else
     {
-      // :todo: implement all the formats of the first argument.
-      L.pushString(c.getTime().toString());
+      StringBuffer b = new StringBuffer();
+      int i = 0;
+      int l = s.length();
+      while (i < l)
+      {
+        char ch = s.charAt(i);
+        ++i;
+        if (ch != '%')
+        {
+          b.append(ch);
+          continue;
+        }
+        if (i >= l)
+        {
+          break;
+        }
+        ch = s.charAt(i);
+        ++i;
+        // Generally in order to save space, the abbreviated forms are
+        // identical to the long forms.
+        // The specifiers are from [C1990].
+        switch (ch)
+        {
+          case 'a': case 'A':
+            b.append(weekdayname(c));
+            break;
+          case 'b': case 'B':
+            b.append(monthname(c));
+            break;
+          case 'c':
+            b.append(c.getTime().toString());
+            break;
+          case 'd':
+            b.append(format(c.get(Calendar.DAY_OF_MONTH), 2));
+            break;
+          case 'H':
+            b.append(format(c.get(Calendar.HOUR), 2));
+            break;
+          case 'I':
+            {
+              int h = c.get(Calendar.HOUR);
+              h = (h+11)%12+1;  // force into range 1-12
+              b.append(format(h, 2));
+            }
+            break;
+          case 'j':
+          case 'U': case 'W':
+            // Not supported because CLDC 1.1 doesn't provide it.
+            b.append('%');
+            b.append(ch);
+            break;
+          case 'm':
+            {
+              int m = canonicalmonth(c.get(Calendar.MONTH));
+              b.append(format(m, 2));
+            }
+            break;
+          case 'M':
+            b.append(format(c.get(Calendar.MINUTE), 2));
+            break;
+          case 'p':
+            {
+              int h = c.get(Calendar.HOUR);
+              b.append(h<12 ? "am" : "pm");
+            }
+            break;
+          case 'S':
+            b.append(format(c.get(Calendar.SECOND), 2));
+            break;
+          case 'w':
+            b.append(canonicalweekday(c.get(Calendar.DAY_OF_WEEK)));
+            break;
+          case 'x':
+            {
+              String u = c.getTime().toString();
+              // We extract fields from the result of Date.toString.
+              // The output of which is of the form:
+              // dow mon dd hh:mm:ss zzz yyyy
+              // except that zzz is optional.
+              b.append(u.substring(0, 11));
+              b.append(c.get(Calendar.YEAR));
+            }
+            break;
+          case 'X':
+            {
+              String u = c.getTime().toString();
+              b.append(u.substring(11, u.length()-5));
+            }
+            break;
+          case 'y':
+            b.append(format(c.get(Calendar.YEAR) % 100, 2));
+            break;
+          case 'Y':
+            b.append(c.get(Calendar.YEAR));
+            break;
+          case 'Z':
+            b.append(tz.getID());
+            break;
+          case '%':
+            b.append('%');
+            break;
+        }
+      } /* while */
+      L.pushString(b.toString());
     }
     return 1;
   }
@@ -201,24 +306,6 @@ public final class OSLib extends LuaJavaCallback {
     Calendar.NOVEMBER,
     Calendar.DECEMBER
   };
-
-  /**
-   * (almost) inverts the conversion provided by {@link OSLib#MONTH}.  Converts
-   * from a {@link Calendar} value to a month in the range 1-12.
-   * @param m  a value from the enum Calendar.JANUARY, Calendar.FEBRUARY, etc
-   * @return a month in the range 1-12, or the original value.
-   */
-  private static int canonicalmonth(int m)
-  {
-    for (int i=0; i<MONTH.length; ++i)
-    {
-      if (m == MONTH[i])
-      {
-        return i+1;
-      }
-    }
-    return m;
-  }
 
   /** Implements setlocale. */
   private static int setlocale(Lua L)
@@ -269,5 +356,79 @@ public final class OSLib extends LuaJavaCallback {
   private static void setfield(Lua L, String key, int value)
   {
     L.setField(L.value(-1), key, L.valueOfNumber(value));
+  }
+
+  /** Format a positive integer in a 0-filled field of width
+   * <var>w</var>.
+   */
+  private static String format(int i, int w)
+  {
+    StringBuffer b = new StringBuffer();
+    b.append(i);
+    while (b.length() < w)
+    {
+      b.insert(0, '0');
+    }
+    return b.toString();
+  }
+
+  private static String weekdayname(Calendar c)
+  {
+    String s = c.getTime().toString();
+    return s.substring(0, 3);
+  }
+
+  private static String monthname(Calendar c)
+  {
+    String s = c.getTime().toString();
+    return s.substring(4, 7);
+  }
+
+  /**
+   * (almost) inverts the conversion provided by {@link OSLib#MONTH}.  Converts
+   * from a {@link Calendar} value to a month in the range 1-12.
+   * @param m  a value from the enum Calendar.JANUARY, Calendar.FEBRUARY, etc
+   * @return a month in the range 1-12, or the original value.
+   */
+  private static int canonicalmonth(int m)
+  {
+    for (int i=0; i<MONTH.length; ++i)
+    {
+      if (m == MONTH[i])
+      {
+        return i+1;
+      }
+    }
+    return m;
+  }
+
+  // DO NOT MODIFY ARRAY
+  private static final int[] WEEKDAY =
+  {
+    Calendar.SUNDAY,
+    Calendar.MONDAY,
+    Calendar.TUESDAY,
+    Calendar.WEDNESDAY,
+    Calendar.THURSDAY,
+    Calendar.FRIDAY,
+    Calendar.SATURDAY,
+  };
+
+  /**
+   * Converts from a {@link Calendar} value to a weekday in the range
+   * 0-6 where 0 is Sunday (as per the convention used in [C1990]).
+   * @param w  a value from the enum Calendar.SUNDAY, Calendar.MONDAY, etc
+   * @return a weekday in the range 0-6, or the original value.
+   */
+  private static int canonicalweekday(int w)
+  {
+    for (int i=0; i<WEEKDAY.length; ++i)
+    {
+      if (w == WEEKDAY[i])
+      {
+        return i;
+      }
+    }
+    return w;
   }
 }
