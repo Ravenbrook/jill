4e49ec2f96edd16fcc30b1dd7fb5b6d94486e497
David Jones <drj@ravenbrook.com>
2006-08-03 16:54:41 +0100

    Jili: correcting readonly.lua accept test.

--- test/mnj/lua/AcceptTest.java
+++ test/mnj/lua/AcceptTest.java
@@ -38,6 +38,30 @@ public class AcceptTest extends JiliTestCase
     L.call(0, 0);
   }
 
+  /**
+   * This test has separate Java code because the correct test outcome
+   * is to generate an error.
+   */
+  public void testreadonly()
+  {
+    Lua L = new Lua();
+    BaseLib.open(L);
+    PackageLib.open(L);
+    MathLib.open(L);
+    OSLib.open(L);
+    StringLib.open(L);
+    TableLib.open(L);
+
+    System.out.println(getName());
+    int status = L.loadFile("accept-basic/" + getName() + ".lua");
+    assertTrue(status == 0);
+    status = L.pcall(0, 0, null);
+    assertTrue("Script raised error", status != 0);
+    String s = (String)L.value(-1);
+    // Error message should contain text from the test.
+    assertTrue("error looks plausible", s.indexOf("redefine") >= 0);
+  }
+
   public Test suite()
   {
     TestSuite suite = new TestSuite();
@@ -50,7 +74,10 @@ public class AcceptTest extends JiliTestCase
     // The output from life.lua is too voluminous to run routinely.  See
     // LifeTest.
     // suite.addTest(new AcceptTest("life"));
-    suite.addTest(new AcceptTest("readonly"));
+    suite.addTest(new AcceptTest("readonly")
+      {
+        public void runTest() { testreadonly(); }
+      });
     suite.addTest(new AcceptTest("sieve"));
     suite.addTest(new AcceptTest("sort"));
 
