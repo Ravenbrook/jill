1d3891758a9380d917085f0abeea1e67dd561e47
David Jones <drj@ravenbrook.com>
2006-08-07 12:39:23 +0100

    Jili: removing table.getn from test

--- test/mnj/lua/accept-advanced/sort.lua
+++ test/mnj/lua/accept-advanced/sort.lua
@@ -3,7 +3,7 @@ print"testing sort"
 
 function check (a, f)
   f = f or function (x,y) return x<y end;
-  for n=table.getn(a),2,-1 do
+  for n=#(a),2,-1 do
     assert(not f(a[n], a[n-1]))
   end
 end
